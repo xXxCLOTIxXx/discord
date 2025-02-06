cbd2972bb34235a983c26faf78e156e4fcd8de17
Xsarz <88231084+xXxCLOTIxXx@users.noreply.github.com>
2025-02-06 23:52:39 +0100

    Delete udiscord/utils/objects/event.py

--- udiscord/utils/objects/event.py
+++
@@ -1,7 +0,0 @@
-from . import UserProfile
-
-class Event:
-    def __init__(self, data: dict, type: str = None):
-        self.data: dict = data
-        self.type: str | None = type
-        self.author: UserProfile = UserProfile(data.get("author", {}))
