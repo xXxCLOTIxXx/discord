1e52b821933ab4a626006f2ce2cfa304a2a83be0
Xsarz <88231084+xXxCLOTIxXx@users.noreply.github.com>
2025-02-08 14:29:01 +0200

    Delete udiscord directory

--- udiscord/utils/objects/event/event.py
+++
@@ -1,5 +0,0 @@
-
-class Event:
-    def __init__(self, data: dict, type: str = None):
-        self.data: dict = data
-        self.type: str | None = type
--- udiscord/ws/__init__.py
+++
@@ -1,3 +0,0 @@
-#from .async_socket import AsyncSocket
-from .socket import Socket
-from ..utils.objects.event.event_types import EventType
