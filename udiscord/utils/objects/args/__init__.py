7467a2043df2ceb38126a4fbcecf216849ebd05c
Xsarz <88231084+xXxCLOTIxXx@users.noreply.github.com>
2025-03-10 18:02:07 +0200

    v1.3.7

---
+++ udiscord/utils/objects/args/__init__.py
@@ -0,0 +1,2 @@
+
+from .audit_action import AuditAction
--- udiscord/utils/requester.py
+++ udiscord/utils/requester.py
@@ -25,14 +25,14 @@ class Requester:
 
 
 
-	def make_request(self, method: str, endpoint: str, body: dict = None, allowed_code: int = 200, proxies: dict = None, headers: dict = None, api: str = None) -> Response:
-		response = self.session.request(method, f"{api or self.web_api}{endpoint}", data=dumps(body) if body is not None else None, headers=self.headers(headers), proxies=proxies)
-		log.debug(f"[https][{method}][{api or ''}{endpoint}][{response.status_code}]: {body}")
+	def make_request(self, method: str, endpoint: str = None, body: dict | bytes = None, allowed_code: int = 200, proxies: dict = None, headers: dict = None, api: str = None) -> Response:
+		response = self.session.request(method, f"{api or self.web_api}{endpoint or ''}", data=dumps(body) if isinstance(body, dict) else body if body is not None else None, headers=self.headers(headers), proxies=proxies)
+		log.debug(f"[https][{method}][{api or ''}{endpoint or ''}][{response.status_code}]: {len(body) if isinstance(body, bytes) else body}")
 		return checkException(response.text) if response.status_code != allowed_code else response
 
 
-	async def make_async_request(self, method: str, endpoint: str, body: dict = None, allowed_code: int = 200, headers: dict = None , api: str = None) -> ClientResponse:
+	async def make_async_request(self, method: str, endpoint: str = None, body: dict = None, allowed_code: int = 200, headers: dict = None , api: str = None) -> ClientResponse:
 		async with ClientSession() as asyncSession:
-			response = await asyncSession.request(method, f"{api or self.web_api}{endpoint}", data=dumps(body) if body else None, headers=self.headers(headers))
-			log.debug(f"[https][{method}][{endpoint}][{response.status}]: {body}")
+			response = await asyncSession.request(method, f"{api or self.web_api}{endpoint or ''}", data=dumps(body) if body else None, headers=self.headers(headers))
+			log.debug(f"[https][{method}][{endpoint or ''}][{response.status}]: {body}")
 			return checkException(await response.text()) if response.status != allowed_code else response
